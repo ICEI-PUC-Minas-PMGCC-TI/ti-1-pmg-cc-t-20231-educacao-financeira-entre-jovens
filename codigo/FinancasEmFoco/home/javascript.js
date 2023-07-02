f6a725468fada93a7efa1b5ddf7af9d213db14fe
Arthur Goncalves de Moraes <gdemoraesarthur@gmail.com>
2023-07-02 19:20:46 -0300

    calendario ok

--- codigo/FinancasEmFoco/home/javascript.js
+++ codigo/FinancasEmFoco/home/javascript.js
@@ -1,3 +1,12 @@
+var selectedDate = null;
+var pins = [];
+
+// Load pins data from localStorage
+var pinsData = localStorage.getItem("pins");
+if (pinsData) {
+  pins = JSON.parse(pinsData);
+}
+
 // grafico
 function reloadPage() {
   location.reload();
@@ -261,152 +270,223 @@ function limparCampos() {
 
 // calendario
 
-var selectedDate = null;
-        var pins = [];
 
-        function pinOnCalendar() {
-            var dateInput = document.getElementById("date");
-            var nameInput = document.getElementById("name");
-            var valorInput = document.getElementById("valor");
 
-            var dateValue = dateInput.value;
-            var nameValue = nameInput.value;
-            var valorValue = valorInput.value;
+function pinOnCalendar() {
+  var dateInput = document.getElementById("date");
+  var nameInput = document.getElementById("name");
+  var valorInput = document.getElementById("valor");
 
-            var date = new Date(dateValue + "T00:00:00");
-            var day = date.getDate();
-            var month = date.getMonth();
-            var year = date.getFullYear();
+  var dateValue = dateInput.value;
+  var nameValue = nameInput.value;
+  var valorValue = valorInput.value;
 
-            var pin = {
-                date: new Date(year, month, day),
-                name: nameValue,
-                valor: valorValue
-            };
+  var date = new Date(dateValue + "T00:00:00");
+  var day = date.getDate();
+  var month = date.getMonth();
+  var year = date.getFullYear();
 
-            pins.push(pin);
+  var pin = {
+    date: new Date(year, month, day),
+    name: nameValue,
+    valor: valorValue,
+  };
 
-            if (selectedDate && selectedDate.getTime() === pin.date.getTime()) {
-                selectedDate = null;
-            } else {
-                selectedDate = pin.date;
-            }
+  pins.push(pin);
 
-            renderCalendar();
-            renderPins();
+  if (selectedDate && selectedDate.getTime() === pin.date.getTime()) {
+    selectedDate = null;
+  } else {
+    selectedDate = pin.date;
+  }
+  
+  renderCalendar();
+renderPins();
 
-            // Clear input values
-            dateInput.value = "";
-            nameInput.value = "";
-            valorInput.value = "";
+// Clear input values
+dateInput.value = "";
+nameInput.value = "";
+valorInput.value = "";
+
+// Save pins data to localStorage
+localStorage.setItem("pins", JSON.stringify(pins));
+}
+
+function previousMonth() {
+  var currentDate = new Date(selectedDate);
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  selectedDate = currentDate;
+  renderCalendar();
+  renderPins();
+}
+
+function nextMonth() {
+  var currentDate = new Date(selectedDate);
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  selectedDate = currentDate;
+  renderCalendar();
+  renderPins();
+}
+
+function renderCalendar() {
+  var calendar = document.getElementById("calendar");
+  var tbody = calendar.getElementsByTagName("tbody")[0];
+  tbody.innerHTML = "";
+
+  var currentDate = selectedDate ? new Date(selectedDate) : new Date();
+
+  var month = currentDate.getMonth();
+  var year = currentDate.getFullYear();
+
+  document.getElementById("monthYear").innerText = new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(currentDate);
+
+  var firstDay = new Date(year, month, 1);
+  var lastDay = new Date(year, month + 1, 0);
+
+  var numRows = Math.ceil((lastDay.getDate() + firstDay.getDay()) / 7);
+  var dayCounter = 1;
+
+  for (var i = 0; i < numRows; i++) {
+    var row = document.createElement("tr");
+    for (var j = 0; j < 7; j++) {
+      var cell = document.createElement("td");
+      if (i === 0 && j < firstDay.getDay()) {
+        cell.innerHTML = "";
+      } else if (dayCounter > lastDay.getDate()) {
+        cell.innerHTML = "";
+      } else {
+        cell.innerHTML = dayCounter;
+        cell.addEventListener("click", onDateClick);
+
+        var pin = pins.find(function(pin) {
+          var pinDate = new Date(pin.date);
+          return pinDate.getFullYear() === year && pinDate.getMonth() === month && pinDate.getDate() === dayCounter;
+        });
+
+        if (pin) {
+          cell.classList.add("pin");
         }
 
-        function previousMonth() {
-            var currentDate = new Date(selectedDate);
-            currentDate.setMonth(currentDate.getMonth() - 1);
-            selectedDate = currentDate;
-            renderCalendar();
-            renderPins();
-        }
+        dayCounter++;
+      }
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
+  }
+}
 
-        function nextMonth() {
-            var currentDate = new Date(selectedDate);
-            currentDate.setMonth(currentDate.getMonth() + 1);
-            selectedDate = currentDate;
-            renderCalendar();
-            renderPins();
-        }
+function renderPins() {
+  var selectedDateContainer = document.getElementById("selectedDate");
+  selectedDateContainer.innerHTML = "";
 
-        function renderCalendar() {
-            var calendar = document.getElementById("calendar");
-            var tbody = calendar.getElementsByTagName("tbody")[0];
-            tbody.innerHTML = '';
+  if (selectedDate) {
+    var selectedDateText = document.createElement("h3");
+    selectedDateText.innerText = "Data Selecionada: " + selectedDate.toDateString();
+    selectedDateContainer.appendChild(selectedDateText);
 
-            var currentDate = selectedDate ? new Date(selectedDate) : new Date();
+    var pinsForDate = pins.filter(function (pin) {
+      return (
+        pin.date.getTime() === selectedDate.getTime() &&
+        pin.date.getMonth() === selectedDate.getMonth() &&
+        pin.date.getFullYear() === selectedDate.getFullYear()
+      );
+    });
 
-            var month = currentDate.getMonth();
-            var year = currentDate.getFullYear();
+    if (pinsForDate.length > 0) {
+      var pinsList = document.createElement("ul");
+      pinsForDate.forEach(function (pin, index) {
+        var listItem = document.createElement("li");
+        var pinInfo = document.createElement("span");
+        pinInfo.innerText =
+          pin.name + ": " + pin.valor + " - " + pin.date.toDateString();
 
-            document.getElementById("monthYear").innerText = new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(currentDate);
+        // Add delete button
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "X";
+        deleteButton.addEventListener("click", function () {
+          removePinByIndex(index);
+        });
 
-            var firstDay = new Date(year, month, 1);
-            var lastDay = new Date(year, month + 1, 0);
+        listItem.appendChild(pinInfo);
+        listItem.appendChild(deleteButton);
+        pinsList.appendChild(listItem);
+      });
+      selectedDateContainer.appendChild(pinsList);
+    } else {
+      var noPinsMessage = document.createElement("p");
+      noPinsMessage.innerText = "No pins for selected date.";
+      selectedDateContainer.appendChild(noPinsMessage);
+    }
+  }
+}
+function onDateClick(event) {
+  var clickedDate = parseInt(event.target.innerHTML);
+  var currentDate = selectedDate ? new Date(selectedDate) : new Date();
+  currentDate.setDate(clickedDate);
+  selectedDate = currentDate;
+  renderCalendar();
+  renderPins();
+}
 
-            var numRows = Math.ceil((lastDay.getDate() + firstDay.getDay()) / 7);
-            var dayCounter = 1;
+function removePinByIndex(index) {
+  if (index >= 0 && index < pins.length) {
+    pins.splice(index, 1);
+    renderCalendar();
+    renderPins();
+    saveCalendarData();
+  }
+}
 
-            for (var i = 0; i < numRows; i++) {
-                var row = document.createElement("tr");
-                for (var j = 0; j < 7; j++) {
-                    var cell = document.createElement("td");
-                    if (i === 0 && j < firstDay.getDay()) {
-                        cell.innerHTML = "";
-                    } else if (dayCounter > lastDay.getDate()) {
-                        cell.innerHTML = "";
-                    } else {
-                        cell.innerHTML = dayCounter;
-                        cell.addEventListener("click", onDateClick);
+function saveCalendarData() {
+  // Convert the date objects to strings before saving to local storage
+  var pinsData = JSON.stringify(
+    pins.map(function(pin) {
+      return {
+        date: pin.date.getTime(),
+        name: pin.name,
+        valor: pin.valor
+      };
+    })
+  );
 
-                        var pin = pins.find(function(pin) {
-                            return pin.date.getFullYear() === year && pin.date.getMonth() === month && pin.date.getDate() === dayCounter;
-                        });
+  localStorage.setItem("pins", pinsData);
+}
 
-                        if (pin) {
-                            cell.classList.add("pin");
-                        }
+function loadCalendarData() {
+  // Load existing calendar data from localStorage
+  var pinsData = localStorage.getItem("pins");
+  if (pinsData) {
+    pins = JSON.parse(pinsData);
 
-                        dayCounter++;
-                    }
-                    row.appendChild(cell);
-                }
-                tbody.appendChild(row);
-            }
-        }
+    // Convert date strings to Date objects
+    pins.forEach(function(pin) {
+      pin.date = new Date(pin.date);
+    });
 
-        function renderPins() {
-            var selectedDateContainer = document.getElementById("selectedDate");
-            selectedDateContainer.innerHTML = '';
+    // Check if selectedDate is in the loaded pins data
+    var selectedDateInPins = pins.find(function(pin) {
+      return (
+        selectedDate &&
+        pin.date &&
+        pin.date.getTime() === selectedDate.getTime()
+      );
+    });
 
-            if (selectedDate) {
-                var selectedDateText = document.createElement("h3");
-                selectedDateText.innerText = "Selected Date: " + selectedDate.toDateString();
-                selectedDateContainer.appendChild(selectedDateText);
+    if (!selectedDateInPins) {
+      // If selectedDate is not found, set it to the date of the first pin
+      selectedDate = pins[0] ? pins[0].date : null;
+    }
+  }
 
-                var pinsForDate = pins.filter(function(pin) {
-                    return pin.date.getTime() === selectedDate.getTime();
-                });
+  // Render calendar and pins
+  renderCalendar();
+  renderPins();
+}
 
-                if (pinsForDate.length > 0) {
-                    var pinsList = document.createElement("ul");
-                    pinsForDate.forEach(function(pin) {
-                        var listItem = document.createElement("li");
-                        var pinInfo = document.createElement("span");
-                        pinInfo.innerText = pin.name + ": " + pin.valor;
-                        var pinDate = document.createElement("span");
-                        pinDate.innerText = pin.date.toDateString();
-                        listItem.appendChild(pinInfo);
-                        listItem.appendChild(document.createElement("br"));
-                        listItem.appendChild(pinDate);
-                        pinsList.appendChild(listItem);
-                    });
-                    selectedDateContainer.appendChild(pinsList);
-                } else {
-                    var noPinsMessage = document.createElement("p");
-                    noPinsMessage.innerText = "No pins for selected date.";
-                    selectedDateContainer.appendChild(noPinsMessage);
-                }
-            }
-        }
 
-        function onDateClick(event) {
-            var clickedDate = parseInt(event.target.innerHTML);
-            var currentDate = selectedDate ? new Date(selectedDate) : new Date();
-            currentDate.setDate(clickedDate);
-            selectedDate = currentDate;
-            renderCalendar();
-            renderPins();
-        }
 
-        // Initial rendering
-        renderCalendar();
-        renderPins();
+// Load existing calendar data from localStorage and render the calendar
+loadCalendarData();
+// Initial rendering
+renderCalendar();
+renderPins();
